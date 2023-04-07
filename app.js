20a5fa4b2d7424474569017f69fd803f3f952050
Sapereza <s.perezanderson@gmail.com>
2023-04-07 18:09:07 -0400

    finished the enite site event the carousel on the landing page. However there is a slight bug with the menu links not working on the landing page only. And ONLY when the landing pages break point is smaller than md. Otherwise everything else works as intended.

--- app.js
+++ app.js
@@ -3,3 +3,42 @@ function Menu(e){
 
     e.name === 'menu' ? (e.name = "close",list.classList.add('top-[80px]') , list.classList.add('opacity-100')) :( e.name = "menu" ,list.classList.remove('top-[80px]'), list.classList.remove('opacity-100'))
 }
+
+
+
+//THE BELOW CODE IS FOR THE CAROUSEL
+
+currentSlideID = 1;
+
+sliderElement = document.getElementById('slider');
+totalSlides = sliderElement.childElementCount;
+
+function next(){
+    if(currentSlideID < totalSlides) {
+        currentSlideID++;
+        showSlide();
+
+    }
+
+}
+
+function prev(){
+    if(currentSlideID > 1){
+        currentSlideID--;
+        showSlide();
+    }
+
+}
+
+function showSlide() {
+    slides = document.getElementById('slider').getElementsByTagName('li')
+    for (let index = 0; index < totalSlides; index++){
+        const element = slides[index];
+        if(currentSlideID===index+1){
+            element.classList.remove('hidden')
+        }else{
+            element.classList.add('hidden')
+        }
+    }
+
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}","./index.html","./app.js","./layout1.html","./layout2.html","./layout3.html","./layout4.html","./layout5.html","./layout6.html"],
+  content: ["./src/**/*.{html,js}","./index.html","./app.js","./layout1.html","./layout2.html","./layout3.html","./layout4.html","./layout5.html","./layout6.html","./images/Layout1.png","./images/Layout2.png","./images/Layout3.png","./images/Layout4.png","./images/Layout5.png","./images/Layout6.png"],
   theme: {
     extend: {
       colors:{
